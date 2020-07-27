2be9a63614bd17e39d53101e0fca9c72e3c1c1e0
lishaobo <516342558@qq.com>
2020-07-27 23:33:04 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# react-form-template-design
+# react-form-templete-design
+用于获取数据的react组件
+
+```shell script
+yarn add react-form-templete-design
+```
+
+# 使用示例
+```jsx
+import FormDesign,{FormRender} from 'react-form-templete-design'
+
+
+//编辑器  获取编辑完成json
+<FormDesign onOver={handleFunc} />
+
+
+<FormRender 
+    submitText="确定"
+    defaultData={{form_item:"render"}}
+    data={"json"}  
+/>
+
+
+```
+
+# API
+
+#### FormDesign
+
+|属性名|说明|类型|默认值|
+|  ---  | ---  | --- | --- |
+|onOver|处理编辑完成状态|function|- |
+
+#### FormRender
+
+|属性名|说明|类型|默认值|
+|  ---  | ---  | --- | --- |
+|submitText|表单提交文字|string| 确定 |
+|defaultData|表单默认数据|object|{}|
+|data|FormDesign onOver 返回结果|string json||
+
+#### 图片编辑功能待补充
---
+++ rollup.config.js
@@ -0,0 +1,84 @@
+import babel from 'rollup-plugin-babel'
+import cssBundle from 'rollup-plugin-css-bundle'
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import resolve from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+import json from '@rollup/plugin-json'
+
+import path from 'path';
+import pkg from './package.json'
+
+
+import React from 'react';
+import ReactIs from 'react-is';
+import ReactDOM from 'react-dom';
+
+export default [
+    {
+        input: 'lib/index.js',
+        output: [
+            {
+                file: pkg.main,
+                format: 'cjs',
+                sourcemap: true
+            },
+            {
+                file: pkg.module,
+                format: 'es',
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            babel( {
+                exclude: 'node_modules/**',
+                runtimeHelpers: true
+            } ),
+            cssBundle( {
+                include: ['**/*.scss'],
+                output: path.resolve( 'dist/style.scss' )
+            } ),
+        ]
+    },
+    {
+        input: 'lib/index.js',
+        output: [
+            {
+                file: pkg.umd,
+                name: "ReactFormTempleteDesign",
+                format: 'umd',
+                sourcemap: true
+            },
+            {
+                file: pkg['umd:min'],
+                name: "ReactFormTempleteDesign",
+                format: 'umd',
+                sourcemap: true,
+                plugins: [terser()]
+            }
+        ],
+        plugins: [
+            external(),
+            babel( {
+                exclude: 'node_modules/**',
+                runtimeHelpers: true
+            } ),
+            cssBundle( {
+                include: ['**/*.scss'],
+                output: path.resolve( 'dist/style.scss' )
+            } ),
+            resolve({
+            }),
+            commonjs({
+                include: /node_modules/,
+                namedExports: {
+                    'react-is': Object.keys(ReactIs),
+                    'react': Object.keys(React),
+                    'react-dom': Object.keys(ReactDOM),
+                //     'styled-components': [ 'styled', 'css', 'ThemeProvider' ]
+                }
+            } ),
+            json()
+        ]
+    }
+]
